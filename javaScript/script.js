59c18b57199baedc39afb50ec46a1db4ee37a1ab
karam069 <storiesmorekaram@gmail.com>
2024-03-14 19:38:23 +0400

    added the first section

---
+++ javaScript/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    let namee = "Loading...";
+
+    let index = 0;
+  
+    function TypeWriter() { 
+     
+     index++;
+ 
+     $("#hone").text(namee.slice(0, index))
+ 
+     index > namee.length ? index = 0 : index = index;
+   
+     }
+ 
+     setInterval(function () {  
+         TypeWriter();
+     }, 200);
+ 
+     window.onload = function () { 
+         $(".loader").css("display", "none");
+      }    
+});
+
+
+$(document).ready(function () { 
+    $(".toggle-button").click(function(){
+        $(".links").toggleClass("active")
+    })
+ });
+
+ $(document).ready(function(){
+    // $("#front-end")
+
+    let Jobname = "Front-End Developer";
+
+    let index = 0;
+  
+    function TypeWriter() { 
+     
+     index++;
+ 
+     $("#front-end").text(Jobname.slice(0, index))
+ 
+     index > Jobname.length ? index = 0 : index = index;
+   
+     }
+ 
+     setInterval(function () {  
+         TypeWriter();
+     }, 200);
+ })
