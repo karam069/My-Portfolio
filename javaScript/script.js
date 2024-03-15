fb26b7f673b0812d52b583bea668a063c3cd2064
karam069 <storiesmorekaram@gmail.com>
2024-03-15 07:44:54 +0400

    commit

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,28 +1,3 @@
-$(document).ready(function(){
-    let namee = "Loading...";
-
-    let index = 0;
-  
-    function TypeWriter() { 
-     
-     index++;
- 
-     $("#hone").text(namee.slice(0, index))
- 
-     index > namee.length ? index = 0 : index = index;
-   
-     }
- 
-     setInterval(function () {  
-         TypeWriter();
-     }, 200);
- 
-     window.onload = function () { 
-         $(".loader").css("display", "none");
-      }    
-});
-
-
 $(document).ready(function () { 
     $(".toggle-button").click(function(){
         $(".links").toggleClass("active")
